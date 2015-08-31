9950b4d348b166a8e4b76bd579022fb26e926c9c
bqqbarbhg <samuli.1995@hotmail.com>
2015-08-31 22:41:54 +0300

    Rename string.h arguments

--- string.h
+++ string.h
@@ -1,44 +1,44 @@
-void *memcpy(void *s1, const void *s2, size_t n);
+void *memcpy(void *dst, const void *src, size_t size);
 
-void *memmove(void *s1, const void *s2, size_t n);
+void *memmove(void *dst, const void *src, size_t size);
 
-char *strcpy(char *s1, const char *s2);
+char *strcpy(char *dst, const char *src);
 
-char *strncpy(char *s1, const char *s2, size_t n);
+char *strncpy(char *dst, const char *src, size_t size);
 
-char *strcat(char *s1, const char *s2);
+char *strcat(char *dst, const char *src);
 
-char *strncat(char *s1, const char *s2, size_t n);
+char *strncat(char *dst, const char *src, size_t size);
 
-int memcmp(const void *s1, const void *s2, size_t n);
+int memcmp(const void *a, const void *b, size_t size);
 
-int strcmp(const char *s1, const char *s2);
+int strcmp(const char *a, const char *b);
 
-int strcoll(const char *s1, const char *s2);
+int strcoll(const char *a, const char *b);
 
-int strncmp(const char *s1, const char *s2, size_t n);
+int strncmp(const char *a, const char *b, size_t size);
 
-size_t strxfrm(char *s1, const char *s2, size_t n);
+size_t strxfrm(char *dst, const char *src, size_t size);
 
-void *memchr(const void *s, int c, size_t n);
+void *memchr(const void *buffer, int value, size_t size);
 
-char *strchr(const char *s, int c);
+char *strchr(const char *str, int value);
 
-size_t strcspn(const char *s1, const char *s2);
+size_t strcspn(const char *str, const char *values);
 
-char *strpbrk(const char *s1, const char *s2);
+char *strpbrk(const char *str, const char *values);
 
-char *strrchr(const char *s, int c);
+char *strrchr(const char *str, int value);
 
-size_t strspn(const char *s1, const char *s2);
+size_t strspn(const char *str, const char *values);
 
-char *strstr(const char *s1, const char *s2);
+char *strstr(const char *str, const char *substring);
 
-char *strtok(char *s1, const char *s2);
+char *strtok(char *str, const char *token);
 
-void *memset(void *s, int c, size_t n);
+void *memset(void *dst, int value, size_t size);
 
 char *strerror(int errnum);
 
-size_t strlen(const char *s);
+size_t strlen(const char *str);
 
